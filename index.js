9a8ec75b329717f2e391757d704f270f29846b9d
Alwiii <210209501011@student.unm.ac.id>
2025-04-08 22:27:41 +0800

    database json

--- index.js
+++ index.js
@@ -2,17 +2,25 @@ const express = require("express");
 const app = express();
 const port = 3000;
 const bodyParser = require("body-parser");
+const db = require("./connect.js");
+const response = require("./response.js");
 
 // routes / URL / Endpoint utama kita method GET
 
 app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
-  res.send("Utama 1");
+  const sql = "SELECT * FROM mhs";
+  db.query(sql, (err, result) => {
+    // hasil data dari mysql
+    response(200, result, "berhasil", res);
+  });
 });
-app.get("/hello", (req, res) => {
-  console.log({ urlParam: req.query });
-  res.send("Hello World!");
+app.get("/find", (req, res) => {
+  const sql = `SELECT nama_lengkap FROM mhs WHERE nim = ${req.query.nim}`;
+  db.query(sql, (err, result) => {
+    response(200, result, "berhasil", res);
+  });
 });
 app.post("/login", (req, res) => {
   console.log({ requestFromOutside: req.body });
